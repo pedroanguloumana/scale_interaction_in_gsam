82ccef4cfc5c210392f9f6b9c489e76c5991101a
Pedro <pedro@Big-Papa.local>
2025-09-12 18:11:49 +0900

    Update figs

--- src/loading.py
+++ src/loading.py
@@ -4,7 +4,7 @@ import xarray as xr
 import xeofs as xe
 # Change below directory to desired location
 #
-BASE_DIRECTORY = './..'
+BASE_DIRECTORY = '../data'
 
 def load_coarse_gsam_w():
     path = BASE_DIRECTORY + '/raw_gsam_data/coarsened_wa/*.wa_*.nc'
